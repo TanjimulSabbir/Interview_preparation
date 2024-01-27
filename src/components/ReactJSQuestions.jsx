39c9e45a8fa3797b19a21dfa05af35cfd45f0ae0
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-28 02:09:36 +0600

    all question added

--- src/components/ReactJSQuestions.jsx
+++ src/components/ReactJSQuestions.jsx
@@ -1,21 +1,24 @@
 import questions from "../assets/questions.json"
 function ReactJSQuestions() {
     return (
-        <div>
-        {questions.ReactJS.map(question => {
-            return (
-                <div key={question.id} className="collapse bg-base-200">
-                    <input type="checkbox" />
-                    <div className="collapse-title text-xl font-medium">
-                        {question.question}
-                    </div>
-                    <div className="collapse-content">
-                        <p>{question.answer}</p>
-                    </div>
-                </div>
-            )
-        })}
-    </div>
+        <div className="w-[80%] md:w-1/2 mx-auto">
+            <h1 className="text-black text-3xl text-center py-4 bg-sky-300 rounded-2xl mt-3">React.JS Most Important Interview Questions</h1>
+            <div className="">
+                {questions.ReactJS.map(question => {
+                    return (
+                        <div key={question.id} className="my-10 bg-gray-300 rounded-2xl py-8 px-4">
+                            <div className="text-xl md:text-3xl mb-4 nunito">
+                                <h2>{question.id}. {question.question}</h2>
+                            </div>
+                            <div className="nunito">
+                                <p>{question.answer}</p>
+                            </div>
+                            <small className="text-green-600 text-end text-xs flex items-end justify-end">React.JS</small>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
