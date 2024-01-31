2d86a6d666e49ffa615c51c8374caa6834d10243
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-31 21:27:37 +0600

    search box adding

--- src/components/JSQuestions.jsx
+++ src/components/JSQuestions.jsx
@@ -2,13 +2,13 @@ import questions from "../assets/questions.json"
 
 function JSQuestions() {
     return (
-        <div className="w-[80%] md:w-2/3 ml-auto md:mr-10 ">
-            <h1 className="text-black text-3xl text-center py-4 bg-sky-300 rounded-2xl mt-3">Javascript Most Important Interview Questions</h1>
+        <div className="w-[80%] md:w-2/3 mx-auto ">
+            <h1 className="text-black text-lg md:text-3xl text-center py-4 *:rounded-2xl mt-3">Javascript Most Important Interview Questions</h1>
 
             <div className="">
                 {questions.Javascript.map(question => {
                     return (
-                        <div id={question.id} key={question.id} className="my-10 bg-gray-300 rounded-2xl py-8 px-4">
+                        <div id={question.id} key={question.id} className="my-3 bg-gray-300 rounded-2xl py-8 px-4">
                             <div className="text-xl md:text-3xl mb-4 nunito">
                                 <h2>{question.id}. {question.question}</h2>
                             </div>
--- src/components/SearchQuestions.jsx
+++ src/components/SearchQuestions.jsx
@@ -1,20 +1,28 @@
+import { RxCross2 } from "react-icons/rx";
+
 function SearchQuestions({ setSearchText, searchText, filteredData }) {
     console.log(filteredData, "from app")
 
     return (
-        <div className="fixed min-h-screen">
-            <div className='flex items-center ml-6'>
-                <input onChange={(event) => setSearchText(event.target.value)} className='border py-3 px-6 border-green-500 rounded-lg w-full'
-                    type="text" value={searchText} placeholder='search questions' />
-            </div>
-            {
-                filteredData?.length > 0 && <div className="absolute top-16 left-2 bg-black text-white rounded">
+        <div className="relative mb-24 h-full w-2/3 mx-auto">
+            <div className="fixed top-4 w-2/3 mx-auto">
+                <div className="relative">
+                    <div className='relative flex items-center'>
+                        <input onChange={(event) => setSearchText(event.target.value)} className='border py-3 px-6 border-green-500 rounded-lg w-full'
+                            type="text" value={searchText} placeholder='search questions' />
+                        <RxCross2 className="absolute right-4 top-4 text-lg" />
+                    </div>
                     {
-                        filteredData?.map(question => (<p key={question.id} className="py-3 px-2 my-2 rounded-md" ><a href={`#${question.id}`}>{question.question}</a></p>))
+                        filteredData?.length > 0 && <div className="absolute top-16 bg-black text-white rounded w-full opacity-65">
+                            {
+                                filteredData?.map(question => (<p key={question.id} className="py-3 px-2 my-2 rounded-md" ><a href={`#${question.id}`}>{question.question}</a></p>))
+                            }
+                        </div>
                     }
                 </div>
-            }
+            </div>
         </div>
+
     )
 }
 
