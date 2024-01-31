e84abf394d41c9321b00fe9e2b6efdbd70141165
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-31 21:32:09 +0600

    search box adding

--- src/components/SearchQuestions.jsx
+++ src/components/SearchQuestions.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 
 function SearchQuestions({ setSearchText, searchText, filteredData }) {
-    console.log(filteredData, "from app")
+    const [open, setOpen] = useState(true)
+
+    const handleCrossClick = () => {
+        setOpen(false);
+        setSearchText("");
+    };
+
+    useEffect(() => {
+        setOpen(true)
+    }, [searchText])
 
     return (
         <div className="relative mb-24 h-full w-2/3 mx-auto">
@@ -10,12 +20,12 @@ function SearchQuestions({ setSearchText, searchText, filteredData }) {
                     <div className='relative flex items-center'>
                         <input onChange={(event) => setSearchText(event.target.value)} className='border py-3 px-6 border-green-500 rounded-lg w-full'
                             type="text" value={searchText} placeholder='search questions' />
-                        <RxCross2 className="absolute right-4 top-4 text-lg" />
+                        <RxCross2 onClick={handleCrossClick} className="absolute right-4 top-4 text-lg cursor-pointer" />
                     </div>
                     {
-                        filteredData?.length > 0 && <div className="absolute top-16 bg-black text-white rounded w-full opacity-65">
+                        open && filteredData?.length > 0 && <div className="absolute top-16 bg-black text-white rounded w-full opacity-65">
                             {
-                                filteredData?.map(question => (<p key={question.id} className="py-3 px-2 my-2 rounded-md" ><a href={`#${question.id}`}>{question.question}</a></p>))
+                                filteredData?.map(question => (<p key={question.id} className="py-3 px-2 my-2 rounded-md" ><a onClick={() => handleCrossClick} href={`#${question.id}`}>{question.question}</a></p>))
                             }
                         </div>
                     }
