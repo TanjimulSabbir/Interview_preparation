c06de62b4877e9919fc2828233984978e545c962
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-02-01 15:03:54 +0600

    list data showing

--- src/components/SearchQuestions.jsx
+++ src/components/SearchQuestions.jsx
@@ -14,13 +14,13 @@ function SearchQuestions({ setSearchText, searchText, filteredData }) {
     }, [searchText])
 
     return (
-        <div className="relative mb-24 md:w-2/3 mx-auto">
+        <div className="relative mb-16 md:w-2/3 mx-auto">
             <div className="fixed top-4 w-full md:w-2/3 mx-auto">
                 <div className="relative">
-                    <div className='relative flex items-center justify-center'>
-                        <input onChange={(event) => setSearchText(event.target.value)} className='border py-3 px-6 border-sky-200 rounded-lg w-full bg-white'
+                    <div className='relative flex items-center justify-center w-[260px] mx-auto'>
+                        <input onChange={(event) => setSearchText(event.target.value)} className='border py-3 px-6 border-green-500 rounded-lg  bg-white'
                             type="text" value={searchText} placeholder='search questions' />
-                        <RxCross2 onClick={handleCrossClick} className="absolute right-4 top-4 text-lg cursor-pointer" />
+                        <RxCross2 onClick={handleCrossClick} className="absolute right-5 top-4 text-lg cursor-pointer text-red-400" />
                     </div>
                     {
                         open && filteredData?.length > 0 && <div className="absolute top-16 min-w-screen min-h-screen inset-0 bg-black opacity-95 rounded text-black">
