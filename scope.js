a4173602dcbc1d54e08c1902e57475cab1e61402
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-02-04 07:56:44 +0600

    array-like object checking

--- scope.js
+++ scope.js
@@ -1,11 +1,23 @@
-var car = "Audi "
+// var car = "Audi "
 
-const Product = () => {
-    const device = "Apple (Mac Book Air)"
-    return function () {
-        return car + device
-    }
+// Product(1, 2, 3)
 
-}
+// const Product = () => {
+//     const device = "Apple (Mac Book Air)"
+//     console.log(arguments.length)
+//     return function () {
+//         return car + device
+//     }
 
-console.dir(Product())
+// }
+
+// function example() {
+//     console.log(arguments); // Arguments object
+//     console.log(arguments.length); // Length property
+//     console.log(arguments[0]); // Accessing elements using indices
+// }
+// example('Hello', 42, true);
+
+const arrayLike = { 0: "Tanjimul", 1: "Islam", 2: "Sabbir", 3: "is a", 4: "software developer" }
+// console.log(Object.keys (arrayLike))
+console.log(Array.from(arrayLike))
